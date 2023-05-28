29527c80615cc0043a5e92a09e6035eb4358a5c6
vidhusan12 <vidhusan709@gmail.com>
2023-05-28 15:07:41 +1200

    Added different firing mode

--- ZombieGame/Assets/Scripts/Weapons/Guns.cs
+++ ZombieGame/Assets/Scripts/Weapons/Guns.cs
@@ -1,40 +1,149 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/*
+ This Script represents a guns controller in a game. it handles
+shooting functionality inculding differnt shooting modes and bullet behavior;
+ */
 
 public class Guns : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    public Transform bulletSpawm;
-    public float bulletVelocity;
-    public float bulletPrefabLifeTime;
+    public Camera playerCamera;
 
+    // Firing
+    public bool isFiring;
+    public bool readyToFire;
+    bool allowReset = true;
+    public float fireDelay = 2f;
+
+    // Burst
+    public int projectilesPerBurst = 3;
+    public int currentBurst;
+
+    // Spread
+    public float spreadIntensity;
+
+    // Projectile 
+    public GameObject projectilePrefab;
+    public Transform projectileSpawn;
+    public float projectileSpeed;
+    public float projectileLifetime = 3f; // seconds
+
+    public enum FiringMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    public FiringMode currentFiringMode;
+
+    // Initialize the gun controller
+    public void Awake()
+    {
+        readyToFire = true;
+        currentBurst = projectilesPerBurst;
+    }
+
+    // Update is called once per frame
     public void Update()
     {
-        //Left mouse click
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        // Check the firing mode and input to determine if firing
+        if (currentFiringMode == FiringMode.Auto)
         {
+            // Holding down the left mouse button
+            isFiring = Input.GetKey(KeyCode.Mouse0);
+        }
+        else if (currentFiringMode == FiringMode.Single || currentFiringMode == FiringMode.Burst)
+        {
+            // Clicking the left mouse button once
+            isFiring = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        // If ready to fire and input is firing, shoot the weapon
+        if (readyToFire && isFiring)
+        {
+            currentBurst = projectilesPerBurst;
             FireWeapon();
         }
     }
 
+    // Fire the weapon
     private void FireWeapon()
     {
-        //Instantiate the bullet
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawm.position, Quaternion.identity);
+        readyToFire = false;
 
-        //Shoot the bullet
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawm.forward.normalized * bulletVelocity, ForceMode.Impulse);
+        // Calculate the firing direction with spread
+        Vector3 firingDirection = CalculateDirectionAndSpread().normalized;
 
-        //Destory the bullet after some time in the air
-        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
-        
+        // Instantiate the projectile
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawn.position, Quaternion.identity);
+
+        // Point the projectile to face the firing direction
+        projectile.transform.forward = firingDirection;
+
+        // Shoot the projectile
+        projectile.GetComponent<Rigidbody>().AddForce(firingDirection * projectileSpeed, ForceMode.Impulse);
+
+        // Destroy the projectile after some time in the air
+        StartCoroutine(DestroyProjectileAfterTime(projectile, projectileLifetime));
+
+        // Check if we are done firing
+        if (allowReset)
+        {
+            Invoke("ResetFire", fireDelay);
+            allowReset = false;
+        }
+
+        // Burst Mode
+        if (currentFiringMode == FiringMode.Burst && currentBurst > 1)
+        {
+            currentBurst--;
+            Invoke("FireWeapon", fireDelay);
+        }
     }
 
-    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    // Reset the firing state
+    private void ResetFire()
+    {
+        readyToFire = true;
+        allowReset = true;
+    }
+
+    // Destroy the projectile after a specified delay
+    private IEnumerator DestroyProjectileAfterTime(GameObject projectile, float delay)
     {
         yield return new WaitForSeconds(delay);
-        Destroy(bullet);
+        Destroy(projectile);
+    }
+
+    // Calculate the firing direction with spread
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        // Fire from the center of the screen to check where we are aiming
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        Vector3 targetPoint;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            //Hitting Something
+            targetPoint = hit.point;
+        }
+        else
+        {
+            //Shooting at the air
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - projectileSpawn.position;
+        float spreadX = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+        float spreadY = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+
+        // Apply spread to the firing direction
+        Vector3 firingDirection = direction + new Vector3(spreadX, spreadY, 0);
+
+        return firingDirection.normalized;
     }
 
 }
